43fd7b42e941a44b65da9917e0871b93f2884b9f
Luong Thi Thuy Diem <21521953@gm.uit.edu.vn>
2023-05-30 20:34:20 +0700

    Sua loi render trang home

--- BGProject/src/app/controllers/SiteController.js
+++ BGProject/src/app/controllers/SiteController.js
@@ -1,29 +1,13 @@
-const TTBG = require('../models/TTBG');
+const ThongtinBG = require('../models/TTBG')
 const Boardgame = require('../models/BG');
 
 class SiteController {
   index(req, res, next) {
-    let results =[]
-    Boardgame.find({}).exec()
-      .then(boardgames => {
-        const boardgameIds = boardgames.map(boardgame => boardgame._id);
-        let datas = []
-        TTBG.find({ _id: { $in: boardgameIds } }).exec()
-          .then(async (ThongtinBGs) => {
-            for (const boardgame of boardgames) {
-              ThongtinBGs.find(async (detail) => {if (detail._id.toString() === boardgame._id.toString()) {
-                await new Promise((resolve) => setTimeout(resolve, 500));
-                datas.push({
-                  detail,boardgame
-                })
-              }});
-            };
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-            res.render('home.handlebars', { datas : datas });
-          })
-          .catch(next);
-      })
-      .catch(next);
+    ThongtinBG.find({}).exec()
+    .then( (ThongtinBGs) => {
+      res.render('home.handlebars', {ThongtinBGs});
+    })
+    .catch(next);
   }
 }
 
