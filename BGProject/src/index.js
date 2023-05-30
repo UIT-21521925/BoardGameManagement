57965ee4a7b0dd764fece94e0ed83460abcde494
Luong Thi Thuy Diem <21521953@gm.uit.edu.vn>
2023-05-30 21:34:04 +0700

    Css voi project thong qua file sass

--- BGProject/src/index.js
+++ BGProject/src/index.js
@@ -6,6 +6,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,'public')))
 // connect to DB
 const db = require('./config/db');
 db.connect();
